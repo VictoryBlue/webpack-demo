5c1822f517be42afd87e8b102b112936bb2d50f8
VictoryBlue <88573273@qq.com>
2023-04-12 00:10:38 +0800

    bug无法显示hello webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    another: './src/another-module.js',
+    // index: './src/index.js',
+    // another: './src/another-module.js',
+    // 解决重复依赖
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared',
+    },
+    another: {
+      import: './src/another-module.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
